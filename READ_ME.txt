44400de2410aab97d018577e87c41923d218cbca
Isabel Tonelli-Sippel <isabelTSmusic@outlook.com>
2020-11-25 03:00:59 -0500

    upload initial files

---
+++ READ_ME.txt
@@ -0,0 +1,2 @@
+To upload computer's copies of the folder type this command:
+scp -r ~/Desktop/Dynamite\ Site/ itonellisippel@csgateway.clarku.edu:~/public_html/
---
+++ menu.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    console.log("document is ready; function started running");
+     
+          $(".burger-nav").on("click", function(){
+                
+                console.log("burger-nav was clicked");
+                $(".burger-nav").toggleClass("open");
+                $(".navbar2 ul li").toggleClass("open");
+                $("#bodypic").toggleClass("open");
+                $("footer").toggleClass("open");
+                $("#calendarcontainer").toggleClass("open");
+                $("#mediabody").toggleClass("open");
+                $("#aboutimg").toggleClass("open");
+                $("#songlist").toggleClass("open");
+                $(".contactform").toggleClass("open");  
+                $(".event_container").toggleClass("open");  
+                $(".contact_div").toggleClass("open");  
+            });        
+                                   
+});
